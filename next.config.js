cac36309db5e214163ee96d80d7e6acdbfdf852b
droplister <dan@droplister.com>
2025-08-21 02:00:31 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/:asset.json',
-        destination: '/api/:asset',
+        destination: '/data/assets/:asset.json',
       },
     ];
   },
